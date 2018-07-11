ede37ab80276187f58775c8144fde52ca8dd28af
Michael LeGrange <mlegrange15@att.net>
2018-07-11 18:15:03 -0500

    fixed the doubling and tripling of the table data with Jackson. Had to move the database.ref outside of the click handler

--- train.js
+++ train.js
@@ -22,50 +22,67 @@ var config = {
     var firstTrainTime = $("#train-time-form").val().trim();
     var frequency = $("#frequency-form").val().trim();
 
+            var currentTime = moment();
+            console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+            var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+            console.log(firstTimeConverted);
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+            console.log("DIFFERENCE IN TIME: " + diffTime);
+            var tRemainder = diffTime % frequency;
+            console.log(tRemainder);
+            var tMinutesTillTrain = frequency - tRemainder;
+            console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+            var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+            console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+            var nextTrainText = (moment(nextTrain).format("hh:mm"));
+            console.log(nextTrainText);
+            console.log(tMinutesTillTrain);
+
     database.ref().push({
         trainName: trainName,
         destination: destination,
         firstTrainTime: firstTrainTime,
         frequency: frequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP,
+        nextTrainText: nextTrainText,
+        tMinutesTillTrain: tMinutesTillTrain,
+
       });
 
     // clears the text out of the form to get ready for a new entry
       $(".form-control").val("")
 
-    // go into the firbase databse and retrieve the info we need to populate our table
-      database.ref().on("child_added", function(childSnapshot) {
+});
 
-        // function that will dynamically add the table rows to the tbody with the corresponding data needed from firebase
-        var createRows = function() {
-            // Get reference to existing tbody then create a new table row element
-            var tBody = $("tbody");
-            var tRow = $("<tr>");
-        
-            
-            var trainNameTable = $("<td>").text(childSnapshot.val().trainName);
-            var destinationTable = $("<td>").text(childSnapshot.val().destination);
-            var frequencyTable = $("<td>").text(childSnapshot.val().frequency);
-            var nextArrivalTable = $("<td>").text("placeholder");
-            var minutesAwayTable = $("<td>").text("placeholder");
+// go into the firbase databse and retrieve the info we need to populate our table
+database.ref().on("child_added", function(childSnapshot) {
 
-            // Append the newly created table data to the table row
-            tRow.append(trainNameTable, destinationTable, frequencyTable, nextArrivalTable, minutesAwayTable);
-            // Append the table row to the table body
-            tBody.append(tRow);
-            // puts the tbody variable onto the page
-            $("#tbody").append(tBody);
 
-            console.log(childSnapshot.val().trainName);
+  // function that will dynamically add the table rows to the tbody with the corresponding data needed from firebase
+  var createRows = function() {
 
-        }
-    // call the createRows function to populate the page
-        createRows();
+
+      // Get reference to existing tbody then create a new table row element
+      var tBody = $("tbody");
+      var tRow = $("<tr>");
+  
       
-      // If any errors are experienced, log them to console.
-      }, function(errorObject) {
-        console.log("The read failed: " + errorObject.code);
-      });
+      var trainNameTable = $("<td>").text(childSnapshot.val().trainName);
+      var destinationTable = $("<td>").text(childSnapshot.val().destination);
+      var frequencyTable = $("<td>").text(childSnapshot.val().frequency);
+      var nextArrivalTable = $("<td>").text(childSnapshot.val().nextTrainText);
+      var minutesAwayTable = $("<td>").text(childSnapshot.val().tMinutesTillTrain);
 
+      // Append the newly created table data to the table row
+      tRow.append(trainNameTable, destinationTable, frequencyTable, nextArrivalTable, minutesAwayTable);
+      // Append the table row to the table body
+      tBody.append(tRow);
+  }
+  // call the createRows function to populate the page
+  createRows();
 
+// If any errors are experienced, log them to console.
+}, function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
 });
